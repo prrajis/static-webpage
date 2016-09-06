15c9840efc1d0a9f718791bdfbb4a5a9134b3e61
RAJESHI2IT <rajesh.p.r@ideas2it.com>
2016-09-07 03:47:11 +0530

    add new static pages

---
+++ README.md
@@ -0,0 +1,11 @@
+###Installation steps  
+* Install node modules  
+  npm install
+* Install bower components  
+  bower install
+
+######Run the server:
+  gulp
+
+######Open the browser and hit:
+http://localhost:8080
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#myModal').on('hidden.bs.modal', function(e) {
+    $('#modalHead').html('');
+    $('#videoPopup').attr('src', '');
+  });
+  $('#myModal').on('show.bs.modal', function(e) {
+    $('#modalHead').html($(e.relatedTarget).find('h3').html());
+    var getDataId = $(e.relatedTarget).find('.cover').attr('data-id');
+    var URL = "//www.youtube.com/embed/"+getDataId+"?rel=0&autoplay=0";
+    $('#videoPopup').attr('src', URL);
+  });
+});
